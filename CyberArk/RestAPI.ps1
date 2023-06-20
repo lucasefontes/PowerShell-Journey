e79055dea10eb063f3f016d3822d15b3e829e309
Lucas Fontes <lucasefontes@gmail.com>
2023-06-20 10:12:56 -0300

    6202023

--- CyberArk/RestAPI.ps1
+++ CyberArk/RestAPI.ps1
@@ -2,9 +2,46 @@
 $AppID = "RESTAPI"
 $Safe = "RESTSAFE"
 $CCPAddress = "192.168.74.155"
-#$Object = "Operating System-WinDomain-test222-test222"
-$Object = "Operating*"
+$Object = "Operating System-WinDomain-test222-test222"
 $Url = "$CCPAddress/AIMWebService/api/Accounts?AppID=$AppID&$Safe=&Object=$Object"
 
 
 Invoke-RestMethod -Method "GET" -Uri $Url
+
+#########################
+
+Clear-Host
+$AppID = Read-Host "Type the AppID(exact name)".Trim()
+
+$Safe = Read-Host "Type the SafeName(exact name)".Trim()
+
+$CCPAddress = Read-Host "Type the CCP Address(FQDN or IP Address)".Trim()
+
+$Object = Read-Host "Type the Object Name(wild card is accepted)".Trim()
+$date = (Get-Date).ToString("MMddyyy")
+$LogFile = "C:\Logs\$env:USERNAME\$date.txt"
+Clear-Host
+Write-Host 
+"---------------------------------"
+"AppID: $AppID"
+"Safe: $Safe"
+"CCP Address: $CCPAddress"
+"Object: $Object"
+"---------------------------------"
+"CCP call being generated in 5 seconds"
+"."
+"."
+"."
+
+for ($second = 1; $second -le 5; $second++)
+{
+    Write-Host "CCP call starting in $second"
+    Start-Sleep -Seconds 1
+}
+
+$Url = "$CCPAddress/AIMWebService/api/Accounts?AppID=$AppID&$Safe=&Object=$Object"
+Clear-Host
+Invoke-RestMethod -Method "GET" -Uri $Url | Tee-Object $LogFile
+
+Write-Host 
+"############# If you need, this CCP call is saved under $LogFile #############"
